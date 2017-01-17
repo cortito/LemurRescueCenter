4193c2da4621ac89d454822371f135289a5a8c5d
root <root@vm-debian>
2017-01-17 14:22:52 +0100

    ajout script

--- ServeurWeb/script/Dwildfly.sh
+++ ServeurWeb/script/Dwildfly.sh
@@ -5,8 +5,13 @@ unzip wildfly-10.1.0.Final.zip
 addgroup wildfly
 useradd -g wildfly wildfly
 chown -R wildfly:wildfly /opt/wildfly-10.1.0.Final
-cp -f ../wildfly/standalone-full.xml /opt/wildfly-10.1.0.Final/standalone/configuration/standalone.xml
-cp -f ../wildfly/wildfly /etc/init.d/
+cd /etc/projet100h/LemurRescueCenter/ServeurWeb
+cp -f wildfly/standalone-full.xml /opt/wildfly-10.1.0.Final/standalone/configuration/standalone-full.xml
+cp -f wildfly/LRC_J2E_9.ear /opt/wildfly-10.1.0.Final/standalone/deployments/
+touch /opt/wildfly-10.1.0.Final/standalone/deployments/LRC_J2E_9.ear.dodeploy
+cp -f wildfly/mysql-connector-java-5.1.40-bin.jar /opt/wildfly-10.1.0.Final/standalone/deployments/
+touch /opt/wildfly-10.1.0.Final/standalone/deployments/mysql-connector-java-5.1.40-bin.jar.dodeploy
+cp -f wildfly/wildfly /etc/init.d/
 chown root:root /etc/init.d/wildfly
 chmod ug+x /etc/init.d/wildfly
-service wildfly start
+/etc/init.d/wildfly
