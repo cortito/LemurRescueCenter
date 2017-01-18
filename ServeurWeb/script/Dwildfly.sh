094653f9e912f1ac37e03889afb6f80c1f469674
root <root@vm-debian>
2017-01-18 07:23:24 +0100

    modif script wildfly

--- ServeurWeb/script/Dwildfly.sh
+++ ServeurWeb/script/Dwildfly.sh
@@ -18,7 +18,9 @@ cp -f wildfly/LRC_J2E_9.ear /opt/wildfly-10.1.0.Final/standalone/deployments/
 touch /opt/wildfly-10.1.0.Final/standalone/deployments/LRC_J2E_9.ear.dodeploy
 cp -f wildfly/mysql-connector-java-5.1.40-bin.jar /opt/wildfly-10.1.0.Final/standalone/deployments/
 touch /opt/wildfly-10.1.0.Final/standalone/deployments/mysql-connector-java-5.1.40-bin.jar.dodeploy
-cp -f wildfly/wildfly /etc/init.d/
+cp /opt/wildfly/docs/contrib/scripts/init.d/wildfly-init-debian.sh /etc/init.d/wildfly
+cp -f wildfly/wildfly /etc/init.d/wildflyperso
 chown root:root /etc/init.d/wildfly
 chmod ug+x /etc/init.d/wildfly
-/etc/init.d/wildfly
+/etc/init.d/wildfly reload
+/etc/init.d/wildfly start
